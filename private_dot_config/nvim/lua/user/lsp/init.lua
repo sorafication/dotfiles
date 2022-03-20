90028487d46ad0ff54e2ec0c1945c66ee151d997
Oktavius Wiesner <git@wiesner.nrw>
2022-03-20 22:55:02 +0100

    Update private_dot_config/nvim/lua/user/lsp/init.lua

--- private_dot_config/nvim/lua/user/lsp/init.lua
+++ private_dot_config/nvim/lua/user/lsp/init.lua
@@ -8,6 +8,6 @@ require("user.lsp.handlers").setup()
 require "user.lsp.null-ls"
 require 'lspconfig'.terraformls.setup{}
 require "user.lsp.settings.terraform"
-require('telescope').load_extension('terraform-doc')
+-- require('telescope').load_extension('terraform-doc')
 
 
