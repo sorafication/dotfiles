fc16bb2da20c8ff5d408256475aecb3d6630e3ba
Oktavius Wiesner <oktavius.wiesner@rtl.de>
2022-10-11 17:22:11 +0200

    Update private_dot_config/lvim/config.lua

--- private_dot_config/lvim/config.lua
+++ private_dot_config/lvim/config.lua
@@ -161,10 +161,11 @@ lvim.builtin.treesitter.highlight.enable = true
 --   },
 -- }
 
-local parser_configs = require("nvim-treesitter.parsers").get_parser_configs()
-parser_configs.hcl = {
-  filetype = "hcl", "terraform",
-}
+-- TERRAFORM STUFF
+-- local parser_configs = require("nvim-treesitter.parsers").get_parser_configs()
+-- parser_configs.hcl = {
+--   filetype = "hcl", "terraform",
+-- }
 
 
 -- -- set additional linters
